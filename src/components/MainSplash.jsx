6c8b54b117513d85f75ef214ba8d0d8b567f84d2
Student <example@example.com>
2019-02-21 13:43:47 -0800

    work on load animation of sliding all content from faded out bottom to top of page on load

--- src/components/MainSplash.jsx
+++ src/components/MainSplash.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Parallax } from 'react-parallax';
 
+
 function MainSplash() {
+
+  const mainSplashAnimate = {
+    position: 'fixed',
+    animation:'3s slide-up',
+    marginTop: '0%'
+  };
+
+
   return (
-    <div>
+    <div style = {mainSplashAnimate}>
       <Parallax
         blur={0}
-        bgImage={require('../assets/images/SplashAlhambra1.jpg')}
+        bgImage= {require('../assets/images/SplashAlhambra1.jpg')}
         bgImageAlt="Alhambra"
         strength={900}
       >
-        <div style={{ height: '500px' }} />
+        <div style={{ height: '200px' }} />
       </Parallax>
       <div class="section white">
         <div class="row container">
@@ -25,7 +34,35 @@ function MainSplash() {
         bgImageAlt="Barcelona"
         strength={900}
       >
-        <div style={{ height: '500px' }} />
+        <div style={{ height: '100px' }} />
+      </Parallax>
+      <div class="section white">
+        <div class="row container">
+          <p class="grey-text text-darken-3 lighten-3">Here is just a little example of what I am working on. This is called a parrallax and it allows me to have two images on your monitor, display the content that I find that is most important to share with you, AND allow my images to still be shared without them losing their place.</p>
+        </div>
+      </div>
+
+      <Parallax
+        blur={0}
+        bgImage={require('../assets/images/SplashBarcelona1.jpg')}
+        bgImageAlt="Barcelona"
+        strength={900}
+      >
+        <div style={{ height: '100px' }} />
+      </Parallax>
+      <div class="section white">
+        <div class="row container">
+          <p class="grey-text text-darken-3 lighten-3">Here is just a little example of what I am working on. This is called a parrallax and it allows me to have two images on your monitor, display the content that I find that is most important to share with you, AND allow my images to still be shared without them losing their place.</p>
+        </div>
+      </div>
+
+      <Parallax
+        blur={0}
+        bgImage={require('../assets/images/SplashBarcelona1.jpg')}
+        bgImageAlt="Barcelona"
+        strength={900}
+      >
+        <div style={{ height: '100px' }} />
       </Parallax>
     </div>
   );
