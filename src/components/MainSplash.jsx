a1cfdd4f92382862724ff2a9229e5468e86ab3bf
Student <example@example.com>
2019-02-26 13:03:11 -0800

    work on parallax on main splash with no luck, try and remove fade in display UI css effect.

--- src/components/MainSplash.jsx
+++ src/components/MainSplash.jsx
@@ -1,31 +1,28 @@
 import React from 'react';
 import ParticlesDisplay from './ParticlesDisplay';
-import { Parallax } from 'react-materialize';
+import { Parallax, Background } from 'react-parallax';
 
 function MainSplash() {
 
-  const mainSplashAnimate = {
-    position: 'relative',
-    transformOrigin: 'bottom',
-    marginTop: '0%'
-  };
-
-
   return (
-    <div style = {mainSplashAnimate}>
+    <div>
       <ParticlesDisplay/>
       <div class = 'container'>
-      <div>
-       <Parallax imageSrc="https://ob9a8415roh4djoj110c31a1-wpengine.netdna-ssl.com/wp-content/uploads/2017/03/alhambra-palace.jpg"/>
-      </div>
-      <div class="section transparent">
-        <h2 className="header white-text">Welcome</h2>
-        <p className="white-text text-darken-3 lighten-3"><em>The more I study, the more insatiable do I feel my genius for it to be. Understand well as I may, my comprehension can only be an infinitesimal fraction of all I want to understand. That brain of mine is something more than merely mortal; as time will show.  -- Ada Lovelace</em></p>
+        <Parallax strength={300}>
+          <Background className="custom-bg">
+            <img src="https://ob9a8415roh4djoj110c31a1-wpengine.netdna-ssl.com/wp-content/uploads/2017/03/alhambra-palace.jpg" alt="fill murray" />
+          </Background>
+        </Parallax>
+        <div class="section transparent">
+          <h2 className="header white-text">Welcome</h2>
+          <p className="white-text text-darken-3 lighten-3"><em>The more I study, the more insatiable do I feel my genius for it to be. Understand well as I may, my comprehension can only be an infinitesimal fraction of all I want to understand. That brain of mine is something more than merely mortal; as time will show.  -- Ada Lovelace</em></p>
         </div>
-      <div>
-       <Parallax imageSrc="https://ob9a8415roh4djoj110c31a1-wpengine.netdna-ssl.com/wp-content/uploads/2017/03/alhambra-palace.jpg"/>
+        <Parallax strength={300}>
+          <Background className="custom-bg">
+            <img src="https://ob9a8415roh4djoj110c31a1-wpengine.netdna-ssl.com/wp-content/uploads/2017/03/alhambra-palace.jpg" alt="fill murray" />
+          </Background>
+        </Parallax>
       </div>
-    </div>
     </div>
   );
 }
